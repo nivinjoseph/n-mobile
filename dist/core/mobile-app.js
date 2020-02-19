7b38205079ccc2fb84c035840eabb7a882a1a04f
nivinjoseph <nivinjoseph@outlook.com>
2020-02-19 08:44:46 -0500

    preparing to publish new version

--- dist/core/mobile-app.js
+++ dist/core/mobile-app.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("@nivinjoseph/n-ext");
 const n_nativescript_vue_1 = require("n-nativescript-vue");
-const n_config_1 = require("@nivinjoseph/n-config");
 const n_exception_1 = require("@nivinjoseph/n-exception");
 class MobileApp {
     constructor() {
@@ -26,19 +25,10 @@ class MobileApp {
         this._isBootstrapped = true;
     }
     configureGlobalConfig() {
-        if (n_config_1.ConfigurationManager.getConfig("env") === "dev") {
-            console.log("Bootstrapping in DEV mode.");
-            n_nativescript_vue_1.default.config.silent = false;
-            n_nativescript_vue_1.default.config.devtools = true;
-            n_nativescript_vue_1.default.config.performance = true;
-            n_nativescript_vue_1.default.config.productionTip = true;
-        }
-        else {
-            n_nativescript_vue_1.default.config.silent = true;
-            n_nativescript_vue_1.default.config.devtools = false;
-            n_nativescript_vue_1.default.config.performance = false;
-            n_nativescript_vue_1.default.config.productionTip = false;
-        }
+        n_nativescript_vue_1.default.config.silent = false;
+        n_nativescript_vue_1.default.config.devtools = true;
+        n_nativescript_vue_1.default.config.performance = true;
+        n_nativescript_vue_1.default.config.productionTip = true;
     }
 }
 exports.MobileApp = MobileApp;

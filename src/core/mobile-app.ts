b1f248f013cf3dfe1cf459fb43664fb5892fc74d
nivinjoseph <nivinjoseph@outlook.com>
2020-02-19 07:38:53 -0500

    more testing

--- src/core/mobile-app.ts
+++ src/core/mobile-app.ts
@@ -1,10 +1,20 @@
+import "@nivinjoseph/n-ext";
 import Vue from "n-nativescript-vue";
+import { ConfigurationManager } from "@nivinjoseph/n-config";
+import { InvalidOperationException } from "@nivinjoseph/n-exception";
 
 
 export class MobileApp
 {
+    private _isBootstrapped = false;
+    
     public bootstrap(): void
     {
+        if (this._isBootstrapped)
+            throw new InvalidOperationException("bootstrap");
+        
+        this.configureGlobalConfig();
+        
         const s = new Vue({
             template: `
                         <Frame>
@@ -17,5 +27,27 @@ export class MobileApp
         });
         
         s.$start();
+        
+        this._isBootstrapped = true;
+    }
+    
+    private configureGlobalConfig(): void
+    {
+        if (ConfigurationManager.getConfig("env") === "dev")
+        {
+            console.log("Bootstrapping in DEV mode.");
+
+            Vue.config.silent = false;
+            Vue.config.devtools = true;
+            Vue.config.performance = true;
+            Vue.config.productionTip = true;
+        }
+        else
+        {
+            Vue.config.silent = true;
+            Vue.config.devtools = false;
+            Vue.config.performance = false;
+            Vue.config.productionTip = false;
+        }
     }
 }

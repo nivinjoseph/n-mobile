06fe342a010004c78161e9e1a9cfaef595513a87
nivinjoseph <nivinjoseph@outlook.com>
2020-02-19 07:39:15 -0500

    preparing to publish new version

--- dist/core/mobile-app.d.ts
+++ dist/core/mobile-app.d.ts
@@ -1,3 +1,6 @@
+import "@nivinjoseph/n-ext";
 export declare class MobileApp {
+    private _isBootstrapped;
     bootstrap(): void;
+    private configureGlobalConfig;
 }
--- dist/core/mobile-app.js
+++ dist/core/mobile-app.js
@@ -1,8 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+require("@nivinjoseph/n-ext");
 const n_nativescript_vue_1 = require("n-nativescript-vue");
+const n_config_1 = require("@nivinjoseph/n-config");
+const n_exception_1 = require("@nivinjoseph/n-exception");
 class MobileApp {
+    constructor() {
+        this._isBootstrapped = false;
+    }
     bootstrap() {
+        if (this._isBootstrapped)
+            throw new n_exception_1.InvalidOperationException("bootstrap");
+        this.configureGlobalConfig();
         const s = new n_nativescript_vue_1.default({
             template: `
                         <Frame>
@@ -14,6 +23,22 @@ class MobileApp {
                     `
         });
         s.$start();
+        this._isBootstrapped = true;
+    }
+    configureGlobalConfig() {
+        if (n_config_1.ConfigurationManager.getConfig("env") === "dev") {
+            console.log("Bootstrapping in DEV mode.");
+            n_nativescript_vue_1.default.config.silent = false;
+            n_nativescript_vue_1.default.config.devtools = true;
+            n_nativescript_vue_1.default.config.performance = true;
+            n_nativescript_vue_1.default.config.productionTip = true;
+        }
+        else {
+            n_nativescript_vue_1.default.config.silent = true;
+            n_nativescript_vue_1.default.config.devtools = false;
+            n_nativescript_vue_1.default.config.performance = false;
+            n_nativescript_vue_1.default.config.productionTip = false;
+        }
     }
 }
 exports.MobileApp = MobileApp;
